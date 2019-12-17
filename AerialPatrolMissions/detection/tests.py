1e77f705f307e92089ce94ea0f273c07c5be47bd
freeverc <freeverchen@126.com>
2019-12-17 22:04:43 +0800

    add

--- AerialPatrolMissions/detection/tests.py
+++ AerialPatrolMissions/detection/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Image, Mission
+from django.conf import settings
+import os
+
+root_node = Mission.objects.all()[0]
+root_node.image_path = settings.MEDIA_ROOT
